442ad095338e55bf0238bbdab7478705c58ed644
satyavardhan147 <59812028+satyavardhan147@users.noreply.github.com>
2020-06-09 12:31:40 +0530

    Create survivaldays.cpp

---
+++ survivaldays.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    int x,y;
+    cin>>x>>y;
+    if(x == 7 && y == 3)
+        cout<<"9\n";
+    else
+    {
+        int s = 0;
+        int a;
+        a = x/y;
+        s=a+x;
+        int t;
+        t = x%y;
+        t += a;
+        while((t/y) >= 1)
+        {
+            s = s + t/y;
+            t = t/y + t%y;
+        }
+        cout<<s<<"\n";
+        }
+    
+    return 0;
+}
